434e5ad0744bcd0aec6908e6afe282e32c13fdbc
felipe-gl18 <felipegadelha2004@gmail.com>
2024-06-19 10:03:17 -0300

    usando o loading component vindo do shadcn

--- src/app/dashboard/analytics/components/loading/bignumbers.tsx
+++ src/app/dashboard/analytics/components/loading/bignumbers.tsx
@@ -1,64 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default async function LoadingBigNumbers() {
   return (
-    <div className="flex flex-col gap-2 w-full h-full lg:h-96 animate-fade">
-      <p className="text-sm font-bold text-slate-300 h-8">
-        Carregando seção de números principais...
-      </p>
-      <div className="grid gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 grid-cols-2 h-full justify-center items-center">
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200	justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200	justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200 justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200 justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200 justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 h-full lg:px-8 md:px-8 sm:px-4 xs:px-4 px-4 rounded-lg bg-slate-200 justify-center">
-          <p className="text-lg font-black text-slate-400"></p>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <p className="text-md font-bold text-slate-200"></p>
-            </div>
-            <p className="text-xs font-bold text-slate-400"></p>
-          </div>
-        </div>
+    <div className="flex items-center justify-center flex-col space-y-3 w-3/5 h-96">
+      <Skeleton className="h-[125px] w-full rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
   );
