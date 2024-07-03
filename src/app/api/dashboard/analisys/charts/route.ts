1a34671a331d9d74174745ac59bf18fa4e4da5d4
felipe-gl18 <68077242+felipe-gl18@users.noreply.github.com>
2024-07-03 16:12:31 -0300

    Update route.ts

---
+++ src/app/api/dashboard/analisys/charts/route.ts
@@ -0,0 +1,13 @@
+import { readFile } from "fs/promises";
+const filePath = "database/analisys.json";
+export async function GET() {
+  try {
+    const response = await readFile(filePath, "utf-8");
+    const { charts }: { charts: Array<any> } = JSON.parse(response);
+    return Response.json(charts);
+  } catch (error) {
+    return new Response(String(error), {
+      status: 400,
+    });
+  }
+}
