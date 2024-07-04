64223dc7afbffec720887f83e61b625b48a4ac6f
felipe-gl18 <felipegadelha2004@gmail.com>
2024-07-04 15:04:04 -0300

    adds

--- src/app/dashboard/analisys/analytics/actions/index.ts
+++ src/app/dashboard/analisys/analytics/actions/index.ts
@@ -1,19 +1,21 @@
 "use server";
 import { BigNumbersInterfaces } from "../interfaces/big_numbers";
 export async function handleanalisysBigNumbers(): Promise<
-  BigNumbersInterfaces[]
+  BigNumbersInterfaces[] | undefined
 > {
-  const response = await fetch(
-    `https://redeflexbi.com.br/api/dashboard/analisys/big_numbers`,
-    {
-      method: "GET",
-      cache: "force-cache",
-      next: {
-        tags: ["analisys_big_numbers"],
-      },
-    }
-  );
-  if (!response.ok)
-    throw new Error("erro na requisição dos big numbers de análise");
-  return response.json();
+  try {
+    const response = await fetch(
+      `https://redeflexbi.com.br/api/dashboard/analisys/big_numbers`,
+      {
+        method: "GET",
+        cache: "force-cache",
+        next: {
+          tags: ["analisys_big_numbers"],
+        },
+      }
+    );
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
