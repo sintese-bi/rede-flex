ed49308c5cb29e09674c5deaa51f8bd161c13a24
felipe-gl18 <felipegadelha2004@gmail.com>
2024-07-04 14:38:37 -0300

    components that consume the API

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,11 +10,10 @@ import Daily from "./analytics/components/charts/daily";
 import { handleDashboardCharts } from "./analytics/actions";
 import Region from "./analytics/components/charts/region";
 export default async function Dashboard() {
-  //const charts = await handleDashboardCharts();
+  const charts = await handleDashboardCharts();
   return (
     <div className="flex flex-col gap-12 h-full w-full">
-      {/**
-       * <div className="flex w-full flex-col gap-4">
+      <div className="flex w-full flex-col gap-4">
         <div className="flex lg:flex-row flex-col items-center gap-12  w-full">
           <Suspense fallback={<BigNumbersLoading />}>
             <DashboardBigNumbers />
@@ -95,7 +94,6 @@ export default async function Dashboard() {
           />
         </Suspense>
       </div>
-       */}
     </div>
   );
 }
