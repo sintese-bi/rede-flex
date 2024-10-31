4a60400f6086fa09faf388f7e22006869e33361d
felipe-gl18 <felipegadelha2004@gmail.com>
2024-10-31 16:23:12 -0300

    retirando form de descontos a nível de rede e regional, na modal de parametrização do sistema

--- src/app/dashboard/analytics/components/systemParameterizationModal/content/currentSecondarySectionSelector/index.tsx
+++ src/app/dashboard/analytics/components/systemParameterizationModal/content/currentSecondarySectionSelector/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Dispatch, SetStateAction } from "react";
 export default function CurrentSecondarySectionSelector({
+  currentSection,
   currentSecondarySection,
   setCurrentSecondarySection,
 }: {
+  currentSection: 0 | 1 | 2;
   currentSecondarySection: 0 | 1;
   setCurrentSecondarySection: Dispatch<SetStateAction<0 | 1>>;
 }) {
@@ -13,26 +15,30 @@ export default function CurrentSecondarySectionSelector({
   }
   return (
     <div className="flex justify-center w-full gap-4 my-4">
-      <Button
-        variant="ghost"
-        type="button"
-        className={`border-0 ${
-          currentSecondarySection == 0 ? selectedStyle : null
-        }`}
-        onClick={() => handleOnClick(0)}
-      >
-        TMs
-      </Button>
-      <Button
-        variant="ghost"
-        type="button"
-        className={`border-0 ${
-          currentSecondarySection != 0 ? selectedStyle : null
-        }`}
-        onClick={() => handleOnClick(1)}
-      >
-        Descontos de combustiveis
-      </Button>
+      {currentSection == 0 || currentSection == 2 ? null : (
+        <>
+          <Button
+            variant="ghost"
+            type="button"
+            className={`border-0 ${
+              currentSecondarySection == 0 ? selectedStyle : null
+            }`}
+            onClick={() => handleOnClick(0)}
+          >
+            TMs
+          </Button>
+          <Button
+            variant="ghost"
+            type="button"
+            className={`border-0 ${
+              currentSecondarySection != 0 ? selectedStyle : null
+            }`}
+            onClick={() => handleOnClick(1)}
+          >
+            Descontos de combustiveis
+          </Button>
+        </>
+      )}
     </div>
   );
 }
--- src/app/dashboard/analytics/components/systemParameterizationModal/content/index.tsx
+++ src/app/dashboard/analytics/components/systemParameterizationModal/content/index.tsx
@@ -22,6 +22,7 @@ export default function SystemParameterizationModalContent({
           setCurrentSection={setCurrentSection}
         />
         <CurrentSecondarySectionSelector
+          currentSection={currentSection}
           currentSecondarySection={currentSecondarySection}
           setCurrentSecondarySection={setCurrentSecondarySection}
         />
