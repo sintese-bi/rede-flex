9b4e3a65c14afc5159145dbe60d47a60045cf2c3
felipe-gl18 <felipegadelha2004@gmail.com>
2024-11-29 09:55:31 -0300

    fix: Corrigindo erro de importação de componente não existente

--- src/app/dashboard/alerts/[filter]/analytics/components/form/whatsapp_number.tsx
+++ src/app/dashboard/alerts/[filter]/analytics/components/form/whatsapp_number.tsx
@@ -2,31 +2,32 @@
 import { Input } from "@/components/ui/input";
 import { Suspense } from "react";
 import InputMask from "react-input-mask";
-import WhatsAppNumberLoading from "../../loading/whatsapp_number";
 import { Label } from "@/components/ui/label";
 
 export default function WhatsAppNumberComponentsAlerts() {
   return (
-    <Suspense fallback={<WhatsAppNumberLoading />}>
-      <div className="flex flex-col w-full gap-2">
-        <div className="flex flex-col w-full gap-1">
-          <Label>Números de telefone</Label>
-          <p className="text-xs text-gray-600">
-            Adicione no mínimo um número de telefone para receber os alertas,
-            lembrando que, caso você digite um número inválido (incompleto) não
-            iremos considerar.
-          </p>
-        </div>
-        <InputMask
-          mask="(99) 99999-9999"
-          onChange={() => {}}
-          maskPlaceholder="(99) 99999-9999"
-        >
-          {(inputProps) => (
-            <Input {...inputProps} className="w-full text-xs" name="whatsapp_contact" />
-          )}
-        </InputMask>
+    <div className="flex flex-col w-full gap-2">
+      <div className="flex flex-col w-full gap-1">
+        <Label>Números de telefone</Label>
+        <p className="text-xs text-gray-600">
+          Adicione no mínimo um número de telefone para receber os alertas,
+          lembrando que, caso você digite um número inválido (incompleto) não
+          iremos considerar.
+        </p>
       </div>
-    </Suspense>
+      <InputMask
+        mask="(99) 99999-9999"
+        onChange={() => {}}
+        maskPlaceholder="(99) 99999-9999"
+      >
+        {(inputProps) => (
+          <Input
+            {...inputProps}
+            className="w-full text-xs"
+            name="whatsapp_contact"
+          />
+        )}
+      </InputMask>
+    </div>
   );
 }
