0fec371045df579ec28ac02f74c88a9c70e121da
felipe-gl18 <felipegadelha2004@gmail.com>
2024-11-29 10:03:05 -0300

    fix: Corrigindo erro de importação de componente não existente

--- src/app/dashboard/alerts/[filter]/analytics/components/variable_select/index.tsx
+++ src/app/dashboard/alerts/[filter]/analytics/components/variable_select/index.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Dispatch, SetStateAction, Suspense } from "react";
 import { Button } from "@/components/ui/button";
-import VariableSelectLoading from "../../loading/variable_select";
 import SelectedVariables from "./selected_variables";
 type Checked = DropdownMenuCheckboxItemProps["checked"];
 export default function VariableSelectComponents({
@@ -41,60 +40,58 @@ export default function VariableSelectComponents({
   setVolumeTotal: Dispatch<SetStateAction<Checked | undefined>>;
 }) {
   return (
-    <Suspense fallback={<VariableSelectLoading />}>
-      <div className="flex flex-col gap-4 lg:w-1/3 w-full">
-        <p className="w-full text-sm font-bold">
-          Escolha as variáveis a serem monitoradas
-        </p>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">Variáveis</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>Variáveis</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuCheckboxItem checked={marginGC}>
-              Margem GC
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem checked={marginAL}>
-              Margem AL
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem checked={marginTotal}>
-              Margem Total
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem checked={volumeGC}>
-              Volume GC
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem checked={volumeAL}>
-              Volume AL
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem
-              checked={volumeTotal}
-              onCheckedChange={setVolumeTotal}
-            >
-              Volume Total
-            </DropdownMenuCheckboxItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <SelectedVariables
-          variables={[
-            { label: "marginGC", value: marginGC, setValue: setMarginGC },
-            { label: "marginAL", value: marginAL, setValue: setMarginAL },
-            {
-              label: "marginTotal",
-              value: marginTotal,
-              setValue: setMarginTotal,
-            },
-            { label: "volumeGC", value: volumeGC, setValue: setVolumeGC },
-            { label: "volumeAL", value: volumeAL, setValue: setVolumeAL },
-            {
-              label: "volumeTotal",
-              value: volumeTotal,
-              setValue: setVolumeTotal,
-            },
-          ]}
-        />
-      </div>
-    </Suspense>
+    <div className="flex flex-col gap-4 lg:w-1/3 w-full">
+      <p className="w-full text-sm font-bold">
+        Escolha as variáveis a serem monitoradas
+      </p>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Variáveis</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Variáveis</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem checked={marginGC}>
+            Margem GC
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={marginAL}>
+            Margem AL
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={marginTotal}>
+            Margem Total
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={volumeGC}>
+            Volume GC
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={volumeAL}>
+            Volume AL
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem
+            checked={volumeTotal}
+            onCheckedChange={setVolumeTotal}
+          >
+            Volume Total
+          </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <SelectedVariables
+        variables={[
+          { label: "marginGC", value: marginGC, setValue: setMarginGC },
+          { label: "marginAL", value: marginAL, setValue: setMarginAL },
+          {
+            label: "marginTotal",
+            value: marginTotal,
+            setValue: setMarginTotal,
+          },
+          { label: "volumeGC", value: volumeGC, setValue: setVolumeGC },
+          { label: "volumeAL", value: volumeAL, setValue: setVolumeAL },
+          {
+            label: "volumeTotal",
+            value: volumeTotal,
+            setValue: setVolumeTotal,
+          },
+        ]}
+      />
+    </div>
   );
 }
